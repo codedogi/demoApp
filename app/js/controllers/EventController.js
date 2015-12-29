9e8b89b00ff9e2fcf992b41add0788a30ee77b42
Rich Spencer <hrspencer@hotmail.com>
2015-12-29 08:41:23 -0600

    Before 2 way binding

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -5,6 +5,8 @@
 
 eventsApp.controller('EventController',
     function($scope) {
+
+        $scope.sortorder = 'name';
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '1/1/2013',
@@ -12,8 +14,43 @@ eventsApp.controller('EventController',
             location: {
                 address: 'Google Headquarters',
                 city: 'Mountain View',
-                province: 'CA'
+                province: 'CA',
+                upVoteCount: 0
             },
-            imageUrl: '/img/angularjs-logo.png'
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass introductory',
+                    creatorName: 'Bob Smith',
+                    duration:1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: 2,
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes. Learn whtat they do, how they do it/ and how to get them to do it for you.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: 4,
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
+                    upVoteCount: 0
+                }
+            ]
+        };
+
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
         }
-    })
+    });
